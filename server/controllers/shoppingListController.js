3f6c360770c7196d480f87ca3a19660e35aeac7b
Fuzumie <Oxyd456852@gmail.com>
2024-11-27 16:24:55 +0100

    .

--- server/controllers/shoppingListController.js
+++ server/controllers/shoppingListController.js
@@ -1,34 +1,31 @@
-const ShoppingList = require('../models/shoppingListModel');
-const User = require('../models/userModel');
+const ShoppingList = require("../models/shoppingListModel");
+const User = require("../models/userModel");
 
 const createList = async (req, res) => {
-  const { name, sharedWith } = req.body; 
+  const { name, sharedWith } = req.body;
   const currentUser = req.user._id;
 
   try {
-    // Ensure the `sharedWith` array contains valid user IDs
     const sharedUsers = await User.find({ _id: { $in: sharedWith } });
 
     if (sharedUsers.length !== sharedWith.length) {
-      return res.status(400).json({ error: 'Some users in sharedWith do not exist' });
+      return res
+        .status(400)
+        .json({ error: "Some users in sharedWith do not exist" });
     }
 
-    // Create the new shopping list
     const newList = new ShoppingList({
       name,
       owner: currentUser,
-      sharedWith, // Add the list to the specified shared users
+      sharedWith,
     });
 
-    // Save the new shopping list
     await newList.save();
 
-    // Add the new list to the current user's `createdLists` array
     const user = await User.findById(currentUser);
     user.createdLists.push(newList._id);
     await user.save();
 
-    // Add the new list to each shared user's `sharedLists` array
     for (const sharedUser of sharedUsers) {
       sharedUser.sharedLists.push(newList._id);
       await sharedUser.save();
@@ -36,8 +33,8 @@ const createList = async (req, res) => {
 
     res.status(201).json(newList);
   } catch (error) {
-    console.error('Error creating list:', error);
-    res.status(500).json({ error: 'Server error', message: error.message });
+    console.error("Error creating list:", error);
+    res.status(500).json({ error: "Server error", message: error.message });
   }
 };
 
@@ -46,42 +43,39 @@ const getListById = async (req, res) => {
 
   try {
     const shoppingList = await ShoppingList.findById(listId)
-      .populate('owner', 'name email')
-      .populate('sharedWith', 'name email');
+      .populate("owner", "name email")
+      .populate("sharedWith", "name email");
 
     if (!shoppingList) {
-      return res.status(404).json({ msg: 'Shopping list not found' });
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
 
     res.json(shoppingList);
   } catch (error) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
-
 
 const getUserShoppingLists = async (req, res) => {
-  const userId = req.user._id
+  const userId = req.user._id;
   try {
     const user = await User.findById(userId)
-      .populate('createdLists', 'name archived')
-      .populate('sharedLists', 'name archived');
-  
+      .populate("createdLists", "name archived")
+      .populate("sharedLists", "name archived");
+
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
-  
+
     res.status(200).json({
       createdLists: user.createdLists,
-      sharedLists: user.sharedLists
+      sharedLists: user.sharedLists,
     });
   } catch (error) {
-    console.error('Error occurred:', error.message);
-    res.status(500).json({ error: 'Server error', details: error.message });
+    console.error("Error occurred:", error.message);
+    res.status(500).json({ error: "Server error", details: error.message });
   }
 };
-
-
 
 const renameList = async (req, res) => {
   const { listId } = req.params;
@@ -91,15 +85,17 @@ const renameList = async (req, res) => {
     const shoppingList = await ShoppingList.findById(listId);
 
     if (!shoppingList) {
-      return res.status(404).json({ msg: 'Shopping list not found' });
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
 
     if (shoppingList.archived) {
-      return res.status(400).json({ msg: 'Cannot update an archived list' });
+      return res.status(400).json({ msg: "Cannot update an archived list" });
     }
 
     if (name && shoppingList.owner.toString() !== req.user._id.toString()) {
-      return res.status(403).json({ msg: 'Only the owner can rename this list' });
+      return res
+        .status(403)
+        .json({ msg: "Only the owner can rename this list" });
     }
 
     if (name) {
@@ -110,7 +106,7 @@ const renameList = async (req, res) => {
 
     res.json(shoppingList);
   } catch (error) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
@@ -122,11 +118,13 @@ const addItemToList = async (req, res) => {
     const shoppingList = await ShoppingList.findById(listId);
 
     if (!shoppingList) {
-      return res.status(404).json({ msg: 'Shopping list not found' });
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
 
     if (shoppingList.archived) {
-      return res.status(400).json({ msg: 'Cannot add items to an archived list' });
+      return res
+        .status(400)
+        .json({ msg: "Cannot add items to an archived list" });
     }
 
     shoppingList.items.push(item);
@@ -134,7 +132,7 @@ const addItemToList = async (req, res) => {
 
     res.json(shoppingList);
   } catch (error) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
@@ -146,19 +144,23 @@ const removeItemFromList = async (req, res) => {
     const shoppingList = await ShoppingList.findById(listId);
 
     if (!shoppingList) {
-      return res.status(404).json({ msg: 'Shopping list not found' });
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
 
     if (shoppingList.archived) {
-      return res.status(400).json({ msg: 'Cannot remove items from an archived list' });
+      return res
+        .status(400)
+        .json({ msg: "Cannot remove items from an archived list" });
     }
 
-    shoppingList.items = shoppingList.items.filter(item => item._id.toString() !== itemId);
+    shoppingList.items = shoppingList.items.filter(
+      (item) => item._id.toString() !== itemId
+    );
     await shoppingList.save();
 
     res.json(shoppingList);
   } catch (error) {
-    res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: "Server error" });
   }
 };
 
@@ -170,27 +172,28 @@ const resolveItem = async (req, res) => {
     const shoppingList = await ShoppingList.findById(listId);
 
     if (!shoppingList) {
-      return res.status(404).json({ msg: 'Shopping list not found' });
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
 
     const item = shoppingList.items.id(itemId);
 
     if (!item) {
-      return res.status(404).json({ msg: 'Item not found in the shopping list' });
+      return res
+        .status(404)
+        .json({ msg: "Item not found in the shopping list" });
     }
 
     item.resolved = !item.resolved;
     await shoppingList.save();
 
     res.json({
-      msg: `Item ${item.resolved ? 'resolved' : 'unresolved'} successfully`,
+      msg: `Item ${item.resolved ? "resolved" : "unresolved"} successfully`,
       shoppingList,
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
-
 
 const archiveList = async (req, res) => {
   const { listId } = req.params;
@@ -199,17 +202,19 @@ const archiveList = async (req, res) => {
     const shoppingList = await ShoppingList.findById(listId);
 
     if (!shoppingList) {
-      return res.status(404).json({ msg: 'Shopping list not found' });
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
 
     if (shoppingList.owner.toString() !== req.user._id.toString()) {
-      return res.status(403).json({ msg: 'Only the owner can archive this list' });
+      return res
+        .status(403)
+        .json({ msg: "Only the owner can archive this list" });
     }
 
     shoppingList.archived = true;
     await shoppingList.save();
 
-    res.json({ msg: 'Shopping list archived successfully', shoppingList });
+    res.json({ msg: "Shopping list archived successfully", shoppingList });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -222,17 +227,21 @@ const deleteList = async (req, res) => {
     const shoppingList = await ShoppingList.findById(listId);
 
     if (!shoppingList) {
-      return res.status(404).json({ msg: 'Shopping list not found' });
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
 
     if (shoppingList.owner.toString() !== req.user._id.toString()) {
-      return res.status(403).json({ msg: 'Only the owner can delete this list' });
+      return res
+        .status(403)
+        .json({ msg: "Only the owner can delete this list" });
     }
 
     await ShoppingList.findByIdAndDelete(listId);
 
     const user = await User.findById(req.user._id);
-    user.createdLists = user.createdLists.filter(listIdRef => listIdRef.toString() !== listId);
+    user.createdLists = user.createdLists.filter(
+      (listIdRef) => listIdRef.toString() !== listId
+    );
     await user.save();
 
     await User.updateMany(
@@ -240,20 +249,20 @@ const deleteList = async (req, res) => {
       { $pull: { sharedLists: listId } }
     );
 
-    res.json({ msg: 'Shopping list deleted successfully' });
+    res.json({ msg: "Shopping list deleted successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
 module.exports = {
-    createList,
-    getListById,
-    getUserShoppingLists,
-    renameList,
-    addItemToList,
-    removeItemFromList,
-    resolveItem,
-    archiveList,
-    deleteList
-}
+  createList,
+  getListById,
+  getUserShoppingLists,
+  renameList,
+  addItemToList,
+  removeItemFromList,
+  resolveItem,
+  archiveList,
+  deleteList,
+};
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,153 +1,152 @@
-const User = require('../models/userModel')
-const ShoppingList = require('../models/shoppingListModel')
-const jwt = require('jsonwebtoken')
+const User = require("../models/userModel");
+const ShoppingList = require("../models/shoppingListModel");
+const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
-  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
-}
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const login = async (req, res) => {
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
   try {
-    const user = await User.login(email, password)
-    const token = createToken(user._id)
-    const id=user._id
-    res.status(200).json({email, token, id})
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    const id = user._id;
+    res.status(200).json({ email, token, id });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-}
+};
 
 const signup = async (req, res) => {
-  const {name, surname, email, password} = req.body
+  const { name, surname, email, password } = req.body;
 
   try {
-    const user = await User.signup(name, surname, email, password)
-    const token = createToken(user._id)
+    const user = await User.signup(name, surname, email, password);
+    const token = createToken(user._id);
 
-    res.status(200).json({email, token})
+    res.status(200).json({ email, token });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    res.status(400).json({ error: error.message });
   }
-}
-
-
+};
 
 const inviteUserToList = async (req, res) => {
-    const { listId } = req.params;
-    const { userId } = req.body;
-  
-    try {
-      // Find the user being invited by email
-      const invitedUser = await User.findById(userId);
-      if (!invitedUser) {
-        return res.status(404).json({ msg: 'Invited user not found' });
-      }
-  
-      // Find the shopping list and check ownership
-      const shoppingList = await ShoppingList.findById(listId);
-      if (!shoppingList) {
-        return res.status(404).json({ msg: 'Shopping list not found' });
-      }
-  
-      // Ensure the current user is the owner of the list
-      if (shoppingList.owner.toString() !== req.user._id.toString()) {
-        return res.status(403).json({ msg: 'You are not the owner of this list' });
-      }
-      
-      // Add the invited user to the sharedWith array if they are not already invited
-      if (shoppingList.sharedWith.includes(invitedUser._id)) {
-        return res.status(400).json({ msg: 'User is already invited' });
-      }
-  
-      shoppingList.sharedWith.push(invitedUser._id);
-      await shoppingList.save();
-  
-      // Update the invited user's shared lists
-      invitedUser.sharedLists.push(shoppingList._id);
-      await invitedUser.save();
-  
-      res.json({ msg: 'User invited successfully' });
-    } catch (error) {
-      console.error("Error occurred:", error);
-      res.status(500).json({ error: error.message });
+  const { listId } = req.params;
+  const { userId } = req.body;
+
+  try {
+    const invitedUser = await User.findById(userId);
+    if (!invitedUser) {
+      return res.status(404).json({ msg: "Invited user not found" });
     }
-  };
+
+    const shoppingList = await ShoppingList.findById(listId);
+    if (!shoppingList) {
+      return res.status(404).json({ msg: "Shopping list not found" });
+    }
+
+    if (shoppingList.owner.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ msg: "You are not the owner of this list" });
+    }
+
+    if (shoppingList.sharedWith.includes(invitedUser._id)) {
+      return res.status(400).json({ msg: "User is already invited" });
+    }
+
+    shoppingList.sharedWith.push(invitedUser._id);
+    await shoppingList.save();
+
+    invitedUser.sharedLists.push(shoppingList._id);
+    await invitedUser.save();
+
+    res.json({ msg: "User invited successfully" });
+  } catch (error) {
+    console.error("Error occurred:", error);
+    res.status(500).json({ error: error.message });
+  }
+};
 
 const getListMembers = async (req, res) => {
-    const { listId } = req.params;
-  
-    try {
-      const shoppingList = await ShoppingList.findById(listId).populate('owner').populate('sharedWith');
-      
-      if (!shoppingList) {
-        return res.status(404).json({ msg: 'Shopping list not found' });
-      }
+  const { listId } = req.params;
 
-      const owner = shoppingList.owner;
-      const members = shoppingList.sharedWith;
-  
-      const result = {
-        owner: {
-          name: owner.name,
-          surname:owner.surname
-        },
-        members: members.map(member => ({
-          name: member.name,
-          surname: member.surname
-        }))
-      };
-  
-      res.json(result);
-    } catch (error) {
-      res.status(500).json({ error: 'Server error' });
+  try {
+    const shoppingList = await ShoppingList.findById(listId)
+      .populate("owner")
+      .populate("sharedWith");
+
+    if (!shoppingList) {
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
-  };
+
+    const owner = shoppingList.owner;
+    const members = shoppingList.sharedWith;
+
+    const result = {
+      owner: {
+        name: owner.name,
+        surname: owner.surname,
+      },
+      members: members.map((member) => ({
+        name: member.name,
+        surname: member.surname,
+      })),
+    };
+
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
 
 const removeUserFromList = async (req, res) => {
-    const { listId } = req.params;
-    const { userIdToRemove } = req.body;  
-  
-    try {
-      const shoppingList = await ShoppingList.findById(listId);
-  
-      if (!shoppingList) {
-        return res.status(404).json({ msg: 'Shopping list not found' });
-      }
-  
-      // Ensure the current user is the owner of the list
-      if (shoppingList.owner.toString() !== req.user._id.toString()) {
-        return res.status(403).json({ msg: 'You are not the owner of this list' });
-      }
-  
-      // Ensure the user being removed is in the sharedWith array
-      if (!shoppingList.sharedWith.includes(userIdToRemove)) {
-        return res.status(400).json({ msg: 'User is not part of this list' });
-      }
-  
-      // Remove the user from the sharedWith array
-      shoppingList.sharedWith = shoppingList.sharedWith.filter(
-        userId => userId.toString() !== userIdToRemove
-      );
-  
-      // Save the updated list
-      await shoppingList.save();
-  
-      // Remove the list from the removed user's sharedLists array
-      const userToRemove = await User.findById(userIdToRemove);
-      if (userToRemove) {
-        userToRemove.sharedLists = userToRemove.sharedLists.filter(
-          listIdRef => listIdRef.toString() !== listId
-        );
-        await userToRemove.save();
-      }
-  
-      res.json({ msg: 'User removed from the list successfully' }, shoppingList);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+  const { listId } = req.params;
+  const { userIdToRemove } = req.body;
+
+  try {
+    const shoppingList = await ShoppingList.findById(listId);
+
+    if (!shoppingList) {
+      return res.status(404).json({ msg: "Shopping list not found" });
     }
-  };
 
+    if (shoppingList.owner.toString() !== req.user._id.toString()) {
+      return res
+        .status(403)
+        .json({ msg: "You are not the owner of this list" });
+    }
 
-module.exports = { signup, login, inviteUserToList, getListMembers, removeUserFromList }
+    if (!shoppingList.sharedWith.includes(userIdToRemove)) {
+      return res.status(400).json({ msg: "User is not part of this list" });
+    }
+
+    shoppingList.sharedWith = shoppingList.sharedWith.filter(
+      (userId) => userId.toString() !== userIdToRemove
+    );
+
+    await shoppingList.save();
+
+    const userToRemove = await User.findById(userIdToRemove);
+    if (userToRemove) {
+      userToRemove.sharedLists = userToRemove.sharedLists.filter(
+        (listIdRef) => listIdRef.toString() !== listId
+      );
+      await userToRemove.save();
+    }
+
+    res.json({ msg: "User removed from the list successfully" }, shoppingList);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  signup,
+  login,
+  inviteUserToList,
+  getListMembers,
+  removeUserFromList,
+};
